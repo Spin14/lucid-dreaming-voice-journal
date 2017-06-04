9d9533dff1ab0662c656a0653e620d1fbf167e17
Valverde <acci.valverde@gmail.com>
2017-06-04 19:37:11 +0100

    new test for dream recording module

--- client/test_client.py
+++ client/test_client.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from os.path import isdir, isfile
 from os import rmdir, remove
 from unittest import TestCase
+from unittest.mock import patch
 
 from client.keyword_responses import get_keyword_response
 
@@ -37,8 +38,12 @@ class TestKeywordResponses(TestCase):
         self.assertEquals(keyword, DREAM_RECORD_KW)
         self.assertEquals(response, record_dream)
 
+def dummy(x,y):
+    pass
 
-from client.dream_recording import gen_filename, save, record_dream
+def dumm2(x):
+    pass
+
 
 class TestDreamRecording(TestCase):
     """Testing dream_recording.py"""
@@ -51,6 +56,8 @@ class TestDreamRecording(TestCase):
             remove(test_file)
 
     def test_gen_filename(self):
+        from client.dream_recording import gen_filename
+
         filename = gen_filename(test_dir)
         expected_filename =\
             '{0}dream_{1}.txt'.format(test_dir, datetime.now().strftime('%y-%m-%d'))
@@ -59,6 +66,8 @@ class TestDreamRecording(TestCase):
         self.assertEquals(filename, expected_filename)
 
     def test_save(self):
+        from client.dream_recording import save
+
         memory_0 = "today i dreamed i had 100% test coverage"
         save(test_file, memory_0)
         memory_1 = "it was then i realized it was just a dream"
@@ -72,6 +81,31 @@ class TestDreamRecording(TestCase):
         self.assertTrue(len(file_memories), 2)
         self.assertEquals(file_memories[0], '{}\n'.format(memory_0))
         self.assertEquals(file_memories[1], '{}\n'.format(memory_1))
+
+    @patch('client.dream_recording.save', side_effect=lambda x, y: None)
+    @patch('client.dream_recording.gen_filename', side_effect=lambda x: None)
+    @patch('client.dream_recording.recognize_audio')
+    def test_record_dream(self, mock_recognize_audio, m_gen_filename, m_save):
+        from client.dream_recording import record_dream
+
+        print(mock_recognize_audio)
+
+        mock_recognize_audio.side_effect = lambda: None
+        record_dream()
+
+        self.assertFalse(record_dream())
+        self.assertFalse(m_save.called)
+        self.assertFalse(m_gen_filename.called)
+
+        mock_recognize_audio.side_effect = lambda: True
+        record_dream()
+
+        self.assertTrue(record_dream())
+        self.assertTrue(m_save.called)
+        self.assertTrue(m_gen_filename.called)
+
+
+
 
 
 
